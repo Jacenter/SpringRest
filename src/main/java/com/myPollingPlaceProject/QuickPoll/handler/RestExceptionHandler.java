5ccd18a886c9643d5828928a88a6d24d415a10b3
Jacenter <JNovack1501@gmail.com>
2021-05-04 15:54:32 -0400

    fixed exception handler

--- src/main/java/com/myPollingPlaceProject/QuickPoll/handler/RestExceptionHandler.java
+++ src/main/java/com/myPollingPlaceProject/QuickPoll/handler/RestExceptionHandler.java
@@ -8,13 +8,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -34,6 +37,31 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         }
 
     @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException manve, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorDetail errorDetail = new ErrorDetail();
+        // Populate errorDetail instance
+        errorDetail.setTimeStamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorDetail.setTitle("Validation Failed");
+        errorDetail.setDetail("Input validation failed");
+        errorDetail.setDeveloperMessage(manve.getClass().getName());
+        // Create ValidationError instances
+        List<FieldError> fieldErrors = manve.getBindingResult().getFieldErrors();
+        for (FieldError fe : fieldErrors) {
+            List<ValidationError> validationErrorList = errorDetail.getErrors().get(fe.getField());
+            if (validationErrorList == null) {
+                validationErrorList = new ArrayList<ValidationError>();
+                errorDetail.getErrors().put(fe.getField(), validationErrorList);
+            }
+            ValidationError validationError = new ValidationError();
+            validationError.setCode(fe.getCode());
+            validationError.setMessage(messageSource.getMessage(fe, null));
+            validationErrorList.add(validationError);
+        }
+        return handleExceptionInternal(manve, errorDetail, headers, status, request);
+    }
+
+    @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         ErrorDetail errorDetail = new ErrorDetail();
         errorDetail.setTimeStamp(new Date().getTime());
@@ -42,16 +70,5 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         errorDetail.setDetail(ex.getMessage());
         errorDetail.setDeveloperMessage(ex.getClass().getName());
         return handleExceptionInternal(ex, errorDetail, headers, status, request);
-    }
-
-    @Override
-    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException manve, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        ErrorDetail errorDetail = new ErrorDetail();
-        errorDetail.setTimeStamp(new Date().getTime());
-        errorDetail.setStatus(status.value());
-        errorDetail.setTitle("Method Argument Not Valid");
-        errorDetail.setDetail(manve.getMessage());
-        errorDetail.setDeveloperMessage(manve.getClass().getName());
-        return handleExceptionInternal(manve, errorDetail, headers, status, request);
     }
 }
